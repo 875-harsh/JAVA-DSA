b8893ba65cecd2ccbe006a5863c5aecba24a5a27
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-03-29 19:23:32 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,13 @@
+public class SelectionSort {
+  public static void main(String[] args) {
+    int arr[]={5,3,8,6,2};
+  }
+  public static void selectionsort(int arr[]){
+    for(int i=0;i<arr.length-1;i++){\
+      int min=i;
+      for(int j=i+1;j<arr.length-1;j++){
+        
+      }
+    }
+  }
+}
